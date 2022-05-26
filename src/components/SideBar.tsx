5096026b268a6351d7637165d6b133c80233f554
Daniel Cazé <danielctlima@gmail.com>
2022-05-26 19:37:41 -0300

    finished components and props

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button"
+import '../styles/sidebar.scss';
+interface sidebarProps {
+  handleClickButton: (id: number) => void
+  selectedGenreId: number
+  genres: Array<{
+    id: number
+    title: string
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'
+  }>
+}
+
+export function SideBar({handleClickButton, genres, selectedGenreId}: sidebarProps) {
+
+  
+  return (
+  <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>
+  )
 }
